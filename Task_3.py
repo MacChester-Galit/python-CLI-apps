ae7e52164802be88c1c1da19843c30723862743c
Chestur <chesseter@gmail.com>
2025-08-28 13:49:39 +0800

    Python CLI App #3 - Odd or Even

---
+++ Task_3.py
@@ -0,0 +1,12 @@
+while True:
+    the_num = input("Enter a number: ")
+    try:
+        the_num = int(the_num)
+        if the_num % 2 == 0:
+            print("The number you entered: " + str(the_num) + " " + "is even.")
+        else:
+            print("The number you entered: " + str(the_num) + " " + "is odd.")
+        break
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+    
